b08f5d0c6590136cd323b18f9808c4536e8773ef
Thiago Soares <devthiagosoares@gmail.com>
2023-10-10 00:32:58 -0400

    ajuste com o que não estava sendo usado

--- src/Cards/card.module.ts
+++ src/Cards/card.module.ts
@@ -2,16 +2,10 @@ import { Module } from '@nestjs/common';
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
 import { PDFModule } from 'src/PDF/pdf.module';
-import { MulterModule } from '@nestjs/platform-express';
 
 // CardModule
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: './uploads', // Diretório onde os arquivos serão salvos temporariamente
-    }),
-    PDFModule,
-  ], // Certifique-se de que PDFModule está importado aqui
+  imports: [PDFModule], // Certifique-se de que PDFModule está importado aqui
   controllers: [CardController],
   providers: [CardService],
 })
