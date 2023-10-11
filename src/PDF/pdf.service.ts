4b8d3ce31a1e6a004ad93d7058a55787e9cdbe5b
Thiago Soares <devthiagosoares@gmail.com>
2023-10-10 21:19:47 -0400

    tranformar imagem em base64 para renderizar no pdf

--- src/PDF/pdf.service.ts
+++ src/PDF/pdf.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import * as ejs from 'ejs';
+import * as fs from 'fs';
 import * as pdf from 'html-pdf';
 import { Payload } from 'src/Cards/dto/createCard.dto';
 
 @Injectable()
 export class PDFService {
   async generatePdf(data: Payload): Promise<string> {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve) => {
       try {
+        const ejsTemplatePath = 'src/Utils/templates/index.ejs';
+        const imageFilePath = 'src/Utils/templates/logo_ICTS.jpg';
+
+        // Certifique-se de que os arquivos existam
+        if (!fs.existsSync(ejsTemplatePath) || !fs.existsSync(imageFilePath)) {
+          return;
+          new Error('O modelo EJS ou a imagem não foram encontrados.');
+        }
+
+        const imageBase64 = fs.readFileSync(imageFilePath, 'base64');
+
+        data.imagePath = imageBase64;
+
         // Carregue o arquivo EJS
         const ejsTemplate = await ejs.renderFile(
           'src/Utils/templates/index.ejs',
@@ -16,7 +30,8 @@ export class PDFService {
 
         const options: pdf.CreateOptions = {
           format: 'Letter',
-          orientation: 'portrait'
+          orientation: 'portrait',
+          border: '10mm'
         };
 
         // Converta o HTML para PDF
@@ -24,13 +39,13 @@ export class PDFService {
           .create(ejsTemplate, options)
           .toFile('./uploads/pedido.pdf', (err, res) => {
             if (err) {
-              reject(err);
+              return err;
             } else {
               resolve(res.filename);
             }
           });
       } catch (error) {
-        reject(error);
+        return error;
       }
     });
   }
